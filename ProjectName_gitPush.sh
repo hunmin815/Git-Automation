31889c28cac2456a07b890656f0e72f3bd78754a
hunmin815 <hunmin815@naver.com>
2021-12-11 19:19:25 +0900

    Init:Initialize Proejct

---
+++ ProjectName_gitPush.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+date=`date`
+github_id=$env_github_id
+github_Token=$env_github_Token
+github_Address=$env_autoTradeGit
+Project_name="autoTrade2"
+logFile="/home/ubuntu/"$Project_name"_push.log"
+SourceDir=$env_autoTradeDir
+
+cd $SourceDir
+
+echo "git add . ..." && echo "git add . ..." > $logFile 2>&1
+echo "`git add .`" >> $logFile 2>&1
+echo "" && echo "" >> $logFile 2>&1 && echo "==" >> $logFile 2>&1
+
+echo "git status" && echo "git status" >> $logFile 2>&1
+echo "`git status`" >> $logFile 2>&1
+echo "" && echo "" >> $logFile 2>&1 && echo "==" >> $logFile 2>&1
+
+echo "git commit -m $date commit" && echo "git commit -m $date commit" >> $logFile 2>&1
+echo "`git commit -m "$date commit"`" >> $logFile 2>&1
+echo "" && echo "" >> $logFile 2>&1 && echo "==" >> $logFile 2>&1
+
+echo "git push!" && echo "git push!" >> $logFile 2>&1
+git push https://$github_id:$github_Token@$github_Address >> $logFile 2>&1
+
+sleep 2
+
+result="`awk '/Everything up-to-date/' $logFile 2>&1`"
+echo "$result"
+
+
+if [ -z "$result" ];then
+	echo "git push Success~!" && echo "git push Success~!" >> $logFile 2>&1
+else
+	echo "!! git push ERROR! please check $logFile !!" && echo "!! git push ERROR! please check $logFile !!" >> $logFile 2>&1
+fi
+
+exit 0
